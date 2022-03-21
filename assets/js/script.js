bef6316e6e90ca6cca8bf857ba6614833613ee91
Eric Kohler <erickohler1600@gmail.com>
2022-03-20 20:55:56 -0400

    Built basic quiz with score and result display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const quiz = document.getElementsByClassName("quiz-section");
 const quizSubmit = document.getElementsByClassName("quiz-submit");
-const start = document.getElementById("start");
+var score = 0;
+
 
 function startQuiz(){
     var num = 0;
@@ -13,23 +14,33 @@ function startQuiz(){
 }
 
 function nextQuiz(num){
+    var displayResult = document.getElementById("result");
+    var isTrue = document.querySelector('#true-'+num).checked;
+    if(validateSubmission(num)){
+        quiz[num].style.display = "none";
+        if(isTrue){
+            displayResult.innerHTML = "Correct";
+            score++;
+        }else displayResult.innerHTML = "Incorrect";
 
-    console.log(document.querySelector('input[name="false-'+num+'"]').checked);
-
-    quiz[num].style.display = "none";
-    quiz[num+1].style.display = "block";
-    quizSubmit[num+1].addEventListener("click", function(event){
-        event.preventDefault();
-        nextQuiz(num+1);
-    });
+        if(num+1>=quiz.length){
+            displayResult.innerHTML = "Final Score: "+score;
+        }else{
+            quiz[num+1].style.display = "block";
+            quizSubmit[num+1].addEventListener("click", function(event){
+                event.preventDefault();
+                nextQuiz(num+1);
+            });
+        }
+    }else alert("Please Select an Option!");
 }
 
 function validateSubmission(num){
-    var radios = document.getElementsByName("false-"+num);
+    var choices = document.getElementsByName("selection-"+num);
 
-    for (var i = 0, len = radios.length; i < len; i++) {
-        if (radios[i].checked) {
-        return true;
+    for (var i = 0, length = choices.length; i < length; i++) {
+        if (choices[i].checked) {
+            return true;
         }
     }
 
