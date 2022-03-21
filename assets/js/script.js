cde16a4bfa011d9c5b96e8348e0d48dd0f1eac40
Eric Kohler <erickohler1600@gmail.com>
2022-03-20 23:37:09 -0400

    Built full quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 const quiz = document.getElementsByClassName("quiz-section");
 const quizSubmit = document.getElementsByClassName("quiz-submit");
-var timeLimit = 75;
+const start = document.getElementById("start");
+const displayResult = document.getElementById("result");
+const displayFinalResult = document.getElementById("finalResult");
+const finalScore = document.getElementById("finalScore");
+const timeLeft = document.getElementById("timeLeft");
+const submitScore = document.querySelector("#scoreSubmit");
+const hsList = document.getElementById("high-scores-list");
+const quizContainer = document.getElementById("quiz-container");
+const hsContainer = document.getElementById("high-score-container");
+var timeLimit = 0;
 var countdown;
 
+updateTime();
+buildHighScoresList();
 
 function startQuiz(){
+    timeLimit = 75;
     var num = 0;
-    document.getElementById("start").style.display = "none";
+    start.style.display = "none";
     quiz[num].style.display = "block";
     updateTime();
     countdown = setInterval(timer, 1000);
@@ -16,8 +28,22 @@ function startQuiz(){
     });
 }
 
+function reset(){
+    timeLimit = 0;
+    updateTime();
+    displayResult.innerHTML = "";
+    displayFinalResult.innerHTML = "";
+    finalScore.style.display = "none";
+    start.style.display = "block";
+    var choices = document.getElementsByTagName('input');
+    for (i = 0; i < choices.length; i++) {
+        if(choices[i].type == 'radio' && choices[i].checked){
+            choices[i].checked = false;
+        }
+    }
+}
+
 function nextQuiz(num){
-    var displayResult = document.getElementById("result");
     var isTrue = document.querySelector('#true-'+num).checked;
     if(validateSubmission(num)){
         quiz[num].style.display = "none";
@@ -30,8 +56,7 @@ function nextQuiz(num){
         }
 
         if(num+1>=quiz.length){
-            stopTime();
-            displayResult.innerHTML = "Final Score: "+timeLimit;
+            endGame();
         }else{
             quiz[num+1].style.display = "block";
             quizSubmit[num+1].addEventListener("click", function(event){
@@ -56,13 +81,75 @@ function validateSubmission(num){
 
 function timer(){
     timeLimit--
-    document.getElementById("timeLeft").innerHTML = timeLimit;
+    timeLeft.innerHTML = timeLimit;
+    if(timeLimit<=0){
+        endGame();
+    }
 }
 
 function updateTime(){
-    document.getElementById("timeLeft").innerHTML = timeLimit;
+    timeLeft.innerHTML = timeLimit;
 }
 
 function stopTime(){
     clearInterval(countdown);
+}
+
+function endGame(){
+    stopTime();
+    for (let index = 0; index < quiz.length; index++) {
+        quiz[index].style.display = "none";        
+    }
+    displayFinalResult.innerHTML = "Final Score: "+timeLimit;
+    finalScore.style.display = "block";
+}
+
+submitScore.addEventListener("click", function(event){
+    event.preventDefault();
+    if(playerName.value == ""){
+        alert("Please enter your name!");
+    }else{
+        var player = {
+            name: playerName.value,
+            score: timeLimit
+        }
+
+        window.localStorage.setItem('player'+Math.floor(Math.random() * 10000), JSON.stringify(player));
+        console.log(getSavedScores());
+        reset();
+    }
+});
+
+function getSavedScores(){
+    var values = [];
+    var keys = Object.keys(localStorage);
+    var i = keys.length;
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]));
+    }
+    return values;
+}
+
+function buildHighScoresList(){
+    var players = getSavedScores();
+    for (let i = 0; i < players.length; i++) {     
+        var player = JSON.parse(players[i]);
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(player.name+": "+player.score));
+        hsList.appendChild(li);
+    }
+}
+
+function viewHighScores(){
+    quizContainer.style.display = "none";
+    hsContainer.style.display = "block";
+    document.getElementById("highscores").style.display = "none";
+    document.getElementById("quiz-game").style.display = "block";
+}
+
+function returnToGame(){
+    quizContainer.style.display = "block";
+    hsContainer.style.display = "none";
+    document.getElementById("highscores").style.display = "block";
+    document.getElementById("quiz-game").style.display = "none";
 }
